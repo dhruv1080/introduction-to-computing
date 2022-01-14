ab8ecc24d8f965a508bd1b5c2cb3aa58cd8e34a6
dhruv1080 <97087996+dhruv1080@users.noreply.github.com>
2022-01-14 15:21:07 +0530

    Add files via upload

---
+++ assignment 1_output.py
@@ -0,0 +1,38 @@
+-------question 1--------
+enter your first no.=45
+enter your second no.=56
+enter your third no.=48
+average= 49.666666666666664 
+thank you
+
+-------question 2--------
+enter your gross income=100000
+enter number of dependents=4
+amount of tax you have to pay= 15600.0
+thank you
+
+-------question 3--------
+Student=[SID, Name, Gender, Course Name, CGPA]
+[21105070, 'Dhruv agrawal', 'M', 'ECE', 10.0] 
+thank you
+
+-------question 4--------
+marks of 5 students:-
+marks before sorting= [50, 80, 70, 90, 60]
+marks after sorting= [50, 60, 70, 80, 90] 
+thank you
+
+-------question 5--------
+---------part-a----------
+before removing 4th element
+color:- ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+after removing 4th element
+color:- ['Red', 'Green', 'White', 'Pink', 'Yellow']
+thank you
+
+-------question 5--------
+---------part-b----------
+color:- ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+after replacing black and pink with purple
+color:- ['Red', 'Green', 'White', 'purple', 'Yellow']
+thank you
